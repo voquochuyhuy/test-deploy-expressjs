3e0ef186df8f2abda5b356fb1f5d2ca6f21ea770
VO QUOC HUY <voquochuy798@gmail.com>
2020-12-29 20:30:17 +0700

    aaa

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import moment from "moment";
 import runQuery from "../databaseConnection";
 var router = express.Router();
 
@@ -7,5 +8,15 @@ router.get('/', async function(req, res, next) {
   const data = await runQuery(`SELECT * FROM log Inner join user on log.userID = user.id`);
   res.send({ data: data });
 });
-
+router.post("/", async function (req, res, next) {
+  const {  userId,action } = req.body;
+  if(!userId) res.status(400);
+  const CreationDate = moment(new Date()).format('YYYY-MM-DD hh:mm:ss');
+  const queryString = `insert into log values (${userId},${action},'${CreationDate})`;
+  const data = await runQuery(queryString);
+  if (data) res.send({ data: data });
+  else {
+    res.status(500);
+  }
+});
 export default router;
