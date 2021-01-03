5e5ac8beb31353a48af6c69ff76d4db6f32f1b65
VO QUOC HUY <voquochuy798@gmail.com>
2021-01-03 12:10:17 +0700

    aaa

--- server/app.js
+++ server/app.js
@@ -17,7 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
-
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 app.use('/', indexRouter);
 app.use('/log', logRouter);
 app.use('/user', userRouter);
