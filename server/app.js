72f0bffc8560352e320b4966a161ab699656b897
VO QUOC HUY <voquochuy798@gmail.com>
2021-01-04 23:31:06 +0700

    aaa

--- server/app.js
+++ server/app.js
@@ -12,18 +12,18 @@ import reportRouter from './routes/report';
 
 var app = express();
 
-// app.use(cors())
+// app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
-app.use( function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "*");
-    next()
-  });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 app.use('/', indexRouter);
 app.use('/log', logRouter);
 app.use('/user', userRouter);
