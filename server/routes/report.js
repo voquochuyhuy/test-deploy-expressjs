206eeb949d81700f012655ff1ea2a6ad786f9566
VO QUOC HUY <voquochuy798@gmail.com>
2021-01-16 11:24:09 +0700

    342

--- server/routes/report.js
+++ server/routes/report.js
@@ -5,16 +5,20 @@ import runQuery from "../databaseConnection";
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  const data = await runQuery(`SELECT report.id,report.message,report.createdAt,question.content,question.id as questionID,user.username FROM report Inner join question on report.postID = question.id inner join user on question.userID = user.id`);
+router.get('/question-report', async function(req, res, next) {
+  const data = await runQuery(`SELECT report.id,report.message,report.createdAt,question.content,question.id as questionID,user.username,user.id as userID FROM report Inner join question on report.targetID = question.id inner join user on question.userID = user.id`);
+  res.send({ data: data });
+});
+router.get('/user-report', async function(req, res, next) {
+  const data = await runQuery(`SELECT report.id,report.message,report.createdAt,user.id as userID,user.username FROM report inner join user on report.targetID = user.id`);
   res.send({ data: data });
 });
 router.post("/", async function (req, res, next) {
-  const { id,userID,postID,message,targetType,type } = req.body;
-  if(!userID) res.status(400);
+  const { id,senderID,targetID,message,targetType,type } = req.body;
+  if(!senderID) res.status(400);
   const CreationDate = moment(new Date()).format('YYYY-MM-DD hh:mm:ss');
   // const id = uuidv4();
-  const queryString = `insert into report (id,userID,postID,targetType,type, message,createdAt) values ('${id}','${userID}','${postID}','${targetType}','${type}','${message}','${CreationDate}');`;
+  const queryString = `insert into report (id,senderID,targetID,targetType,type, message,createdAt) values ('${id}','${senderID}','${targetID}','${targetType}','${type}','${message}','${CreationDate}');`;
   const data = await runQuery(queryString);
   if (data) res.send({ data: data });
   else {
