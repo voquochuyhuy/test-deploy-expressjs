b6f8d61bba807cf307fcfdfcf18b0854017b2ca8
VO QUOC HUY <voquochuy798@gmail.com>
2021-02-09 18:43:39 +0700

    a

--- server/routes/report.js
+++ server/routes/report.js
@@ -19,12 +19,16 @@ router.get('/answer-report', async function(req, res, next) {
 });
 router.get('/dashboard', async function(req, res, next) {
   const CreationDate = moment(new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)).format('YYYY-MM-DD');
-  const totalActiveUser = await runQuery(`SELECT COUNT(*) FROM user WHERE status = 'active' and DATE(createdAt) > ${CreationDate};`);
-  const totalSession = await runQuery(`SELECT COUNT(*) FROM log WHERE DATE(createdAt) > ${CreationDate};`);
+  const _totalActiveUser = await runQuery(`SELECT COUNT(*) FROM user WHERE status = 'active' and DATE(createdAt) > ${CreationDate};`);
+  const _totalSession = await runQuery(`SELECT COUNT(*) FROM log WHERE DATE(createdAt) > ${CreationDate};`);
+  const totalActiveUser = await runQuery(`SELECT COUNT(*) FROM user;`);
+  const totalSession = await runQuery(`SELECT COUNT(*) FROM log;`);
   const oldestLog = await runQuery(`SELECT * FROM log ORDER BY createdAt ASC LIMIT 1`); 
   const numberOfLog = await runQuery(`SELECT COUNT(*) FROM log`);
   console.log(totalActiveUser,totalSession,oldestLog,numberOfLog);
   res.send({ data: {
+    _totalActiveUser,
+    _totalSession,
     totalActiveUser,
     totalSession,
     oldestLog,
